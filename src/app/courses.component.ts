4265647f78982aca18bb81d76c00bb4e0480072d
Niloofar Keshavarz <niloofar.keshavarz@gmail.com>
2023-10-25 16:02:24 -0400

    implements Course component

---
+++ src/app/courses.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({ // a metadata decorator to show that it is a COMPONENT
+    selector : 'courses',
+    template : '<h2> Courses </h2>'
+})
+export class CoursesComponent{
+
+}
